6a564ece6e4f7e016da4ab6980dc3b66a25f5476
yangjiyuan <yangjiyuan@YangJiyuandeMacBook-Pro.local>
2015-04-12 11:00:43 +0800

    电影详情页

--- routes/movie.js
+++ routes/movie.js
@@ -4,7 +4,20 @@ var router = express.Router();
 // movie detail
 router.get('/m/:id',function(req,res){
 	res.render('detail',{
-		title:"movie detail:"+req.params.id
+		title:"电影详情页-机械战警",
+		// flash:http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf
+		movie:{
+			title:'机械战警',
+			doctor:'何塞·帕迪里亚',
+			country:'美国',
+			actors:['乔尔·金纳曼','加里·奥德曼','迈克尔·基顿','艾比·考尼什'],
+			language:'英语',
+			category:'动作/科幻/惊悚/犯罪',
+			poster:'http://a.hiphotos.baidu.com/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=83f58e674d4a20a425133495f13bf347/dc54564e9258d109733d7fc7d358ccbf6d814ded.jpg',
+			flash:'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',			
+			year:'2014',
+			summary:'2028年，专事军火开发的机器人公司Omni Corp生产了大量装备精良的机械战警，他们被投入到维和和惩治犯罪等行动中，取得显著的效果。罪犯横行的底特律市，嫉恶如仇、正义感十足的警察亚历克斯·墨菲（乔尔·金纳曼饰）遭到仇家暗算，身体受到毁灭性破坏。借助于Omni公司天才博士丹尼特·诺顿（加里·奥德曼饰）最前沿的技术，墨菲以机械战警的形态复活。数轮严格的测试表明，墨菲足以承担起维护社会治安的重任，他的口碑在民众中直线飙升，而墨菲的妻子克拉拉（艾比·考尼什饰）和儿子大卫却再难从他身上感觉亲人的温暖。感知到妻儿的痛苦，墨菲决心向策划杀害自己的犯罪头子展开反击。'
+		}
 	})
 });
 
